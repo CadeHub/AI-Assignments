bf5dc13915d9abc97494ca9b1305829a4ead7a6a
CadeHub <cadembradshaw@gmail.com>
2023-04-04 15:17:51 -0500

    Added assignment 3.1

--- Assignment 2.2/main.py
+++ Assignment 2.2/main.py
@@ -7,6 +7,8 @@ csp = {
 }
 
 DEFAULT_VAR_DOMAINS = {}
+PREV_VAR_DOMAINS = {}
+IS_FC = False
 
 
 def readInputFile(filename: str, callback):
@@ -45,10 +47,10 @@ def selectVariable(csp):
 
     mcv = getMostConstrainedVariable(csp, unassigned_variables)
     if len(mcv) > 1:  # tie between values
-        print(f"most constrained h: {mcv}")
+        # print(f"most constrained variable: {mcv}")
         # find most constraining variable (return array with corresponding constraining values)
         mcv = getMostConstrainingVariable(csp, mcv)
-        print(f"most constraining h: {mcv}")
+        # print(f"most constraining variable: {mcv}")
 
         # if there is a tie, sort alphabetically, choose first
         if len(mcv) > 1:
@@ -72,7 +74,7 @@ def getMostConstrainedVariable(csp, var_choices):
         # if there is a tie
         elif curr_domain_len == num_constraints:
             mcv.append(var)
-
+    # print(f"most constrained variable: {mcv}")
     return mcv
 
 
@@ -83,7 +85,11 @@ def getMostConstrainingVariable(csp, var_choices):
     for var in var_choices:
         num_constraints = 0
         for constraint in csp["constraints"]:
-            if var in constraint.values():
+            # if current variable involved in current constraint AND other var in constraint is unassigned
+            other_var = [x for x in constraint.values() if x.isalpha()
+                         and x != var]
+            other_var = other_var[0]
+            if var in constraint.values() and other_var not in csp["state"].keys():
                 num_constraints += 1
 
         if num_constraints > max_constraints:
@@ -92,39 +98,74 @@ def getMostConstrainingVariable(csp, var_choices):
             max_constraints = num_constraints
         elif num_constraints == max_constraints:
             mcv.append(var)
-
     return mcv
 
 
-def selectValue(csp, v):
-    # find least constraining value for variable v # if forward checking enabled, make sure to choose from that list
-    # if tie exists between values:
-        # choose smaller value
-    return  # value chosen (if none available, return null)
+def getLeastConstrainedValue(csp, var):
+    return sortByConstraints(csp, var)  # sortByRemainingDomain(csp, var)
 
 
-def getLeastConstrainedValue(variable, domain):
-    # Sort values in domain by their number of constraints
-    # values = domain[variable]
-    # sorted_values = sortByConstraints(variable, values)
+def getDomainAfterAssignment(csp, var, val, unassigned_var):
+    remaining_domain = csp["variable_domains"][unassigned_var].copy()
 
-    return  # sorted_values
+    # loop through constraints
+    for constraint in csp["constraints"]:
+        # if both current var and unassigned_var involved in constraint
+        if var in constraint.values() and unassigned_var in constraint.values():
+            # loop through remaining domain for the unassigned variable
+            for potential_val in remaining_domain:
+                curr_state = {var: val, unassigned_var: potential_val}
+
+                # if constraint is violated with potential val
+                if checkConstraintViolation(constraint, curr_state):
+                    # remove potential val from remaining domain
+                    remaining_domain.remove(potential_val)
+
+    return remaining_domain
 
 
-def sortByConstraints(variable, values):
-    # Count the number of constraints for each value
-    # constraint_counts = []
-    # for value in values:
-        # count = 0
-        # for constraint in constraints:
-            # if constraint.involves(variable) and constraint.is_violated(value):
-                # count += 1
-        # constraint_counts[value] = count
+def sortByRemainingDomain(csp, var):
+    domain = csp["variable_domains"][var]
+    unassigned_variables = getUnassignedVariables(csp)
+    unassigned_variables.remove(var)
 
-    # Sort values by their constraint count
-    # sorted_values = sort(values, key=lambda value: constraint_counts[value])
+    remaining_choices = {}
 
-    return  # sorted_values
+    # loop through domain of current variable
+    for val in domain:
+        # for each remaining unassigned variable
+        for unassigned_var in unassigned_variables:
+            # if var is assigned to current val
+            # how large is remaining domain
+            remaining_choices[val] = len(
+                getDomainAfterAssignment(csp, var, val, unassigned_var))
+
+    sorted_values = sorted(remaining_choices.items(), key=lambda x: x[1])
+    sorted_values = [val[0] for val in sorted_values]
+    # print(sorted_values)
+    return sorted_values
+
+# sort values in domain by their number of constraints
+
+
+def sortByConstraints(csp, var):
+    potential_values = csp["variable_domains"][var]
+    constraints = csp["constraints"]
+
+    constraint_counts = {}  # num constraints per value
+
+    for val in potential_values:
+        count = 0
+        for constraint in constraints:
+            # if consraint is violated by value, increase count
+            if var in constraint.values() and not checkConstraintViolations(csp):
+                count += 1
+        constraint_counts[val] = count
+
+    # sort values by their constraint count
+    sorted_values = sorted(constraint_counts.items(), key=lambda x: x[1])
+    sorted_values = [val[0] for val in sorted_values]
+    return sorted_values
 
 
 ''' RUNTIME FUNCTIONS '''
@@ -158,20 +199,46 @@ def checkConstraintViolation(constraint, state):
     return not violated
 
 
-def checkConstraintViolations(csp):
+def checkConstraintViolations(csp, var=None, val=None):
     violated = False
+    state = csp["state"].copy()
+
+    state[var] = val
+    # if var != None:
+    #     print(f"Checking for constraint with new state {state}")
 
     # loop through csp constraints that involve var (we only bother checking most recently assigned variable, assuming previous set values are valid)
-    for var in csp["state"].keys():
-        for constraint in csp["constraints"]:
-            # if current variable is a part of the constraint we're looking at
-            if constraint["VAR1"] == var or constraint["VAR2"] == var:
-                # check for violation
-                violated = checkConstraintViolation(constraint, csp["state"])
+    for constraint in csp["constraints"]:
+        # if current variable is a part of the constraint we're looking at
+        if var in constraint.values():
+            # check for violation
+            violated = checkConstraintViolation(constraint, state)
 
-                if violated:  # if at any point we make a violation, return then that it occurred
-                    return violated
+            if violated:  # if at any point we make a violation, return then that it occurred
+                # print(f"violation occured with {var}={val}")
+                return violated
     return violated
+
+
+def updateDomain(csp, var, val):
+    # if IS_FC:
+    #     # loop through the domain of each unassigned variable
+    #     for unassigned_var in getUnassignedVariables(csp):
+    #         for unassigned_val in csp["variable_domains"][unassigned_var]:
+
+    #             # loop through all constraints
+    #             for constraint in csp["constraints"]:
+    #                 # if constraint involves var and violation occurs
+    #                 if var in constraint.values() and checkConstraintViolation(csp, var, val):
+    #                 print()
+    # else:
+    csp["variable_domains"][var].remove(val)
+    return
+
+
+def resetVariableDomain(csp, var):
+    csp["variable_domains"][var] = DEFAULT_VAR_DOMAINS[var].copy()
+    return
 
 
 def checkComplete(csp):  # return true if no constraints violated, and all variables assigned
@@ -180,15 +247,20 @@ def checkComplete(csp):  # return true if no constraints violated, and all varia
     return ((not constraints_violated) and (unassigned_variable_cnt == 0))
 
 
-def outputCurrentBranch(csp, complete):
+def outputCurrentBranch(csp, complete, failed_assignment=None):
     output_str = ""
     total_variable_cnt = len(DEFAULT_VAR_DOMAINS.keys())
     unassigned_variable_cnt = len(getUnassignedVariables(csp))
     assigned_variable_cnt = total_variable_cnt - unassigned_variable_cnt
     cnt = 0
+    state = csp["state"].copy()
+
+    if failed_assignment:
+        state[failed_assignment[0]] = failed_assignment[1]
+        assigned_variable_cnt += 1
 
     # loop through current state
-    for var, val in csp["state"].items():
+    for var, val in state.items():
         if(val != None):
             output_str += f"{var}={val}"
 
@@ -196,7 +268,7 @@ def outputCurrentBranch(csp, complete):
             if(cnt < assigned_variable_cnt - 1):
                 output_str += ", "
             else:
-                output_str += "\tsolution" if (complete) else "\tfailure"
+                output_str += "  solution" if (complete) else "  failure"
 
         cnt += 1
     print(output_str)
@@ -209,48 +281,47 @@ def outputCurrentBranch(csp, complete):
 def solveCSP(csp):
     complete = checkComplete(csp)
     if complete:
-        # TODO: remove
-        print("COMPLETED!")
-        return
+        outputCurrentBranch(csp, complete)
+        return True
 
-    # var <- Select-Unassigned-Variable(assignment, constraints)
-    # note, we'll need to update domain accordingly after choice
     var = selectVariable(csp)
 
-    # for value in Order-Domain-Values(var, assignment, constraints):
-    #     if value satisfies constraints with assignment:
-    #         assignment[var] <- value
-    #         inference <- Inference(assignment, constraints, var, value)
-    #         if inference is not failure:
-    #             result <- Backtracking-Search(assignment + inference, constraints)
-    #             if result is not failure:
-    #                 return result
-    #     assignment[var] <- unassigned
-    #     Restore-Domain-Values(var)
-    #     outputCurrentBranch(csp, complete)
+    for val in getLeastConstrainedValue(csp, var):
+        # print(f"trying {var}={val}")
+        # if no constraints violated with assignment
+        if not checkConstraintViolations(csp, var, val):
+            csp["state"][var] = val
 
-    # return failure
-    return
+            # update domain following assignment
+            updateDomain(csp, var, val)
+            # print(f"updated domains: {csp['variable_domains']}")
+
+            result = solveCSP(csp)
+
+            if result == True:
+                return result
+
+            # remove var assignment
+            csp["state"].pop(var)
+        else:
+            failed_assignment = (var, val)
+            outputCurrentBranch(csp, False, failed_assignment)
+    return False
 
 
 ''' Main Code: '''
 if __name__ == "__main__":
+    # read in command line arguments
     variable_file = sys.argv[1]
     constraint_file = sys.argv[2]
-    isForwardChecking = True if (len(sys.argv) == "fc") else False
+    IS_FC = True if (sys.argv[3] == "fc") else False
 
+    # read in files
     readInputFile(variable_file, processVariablesFile)
     readInputFile(constraint_file, processConstraintFile)
+
     # the domains will be modified if using forward checking, we'll need to reset the domains on backtrack
     DEFAULT_VAR_DOMAINS = csp["variable_domains"]
 
-    # print(csp)
-    # print(isForwardChecking)
-    # csp["state"]["Z"] = 1
-    # csp["state"]["X"] = 0
-    # csp["state"]["Y"] = 0
-
-    # outputCurrentBranch(csp, False)
-    # print(f"Violations: {checkConstraintViolations(csp)}")
-    # print(f"Complete: {checkComplete(csp)}")
-    # print(selectVariable(csp))
+    # main code
+    solveCSP(csp)
