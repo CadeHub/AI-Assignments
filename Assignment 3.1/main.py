1e3d4e80fd64023e626fce482032606de9e3ed4a
CadeHub <cadembradshaw@gmail.com>
2023-04-08 16:12:12 -0500

    Finished

--- Assignment 3.1/main.py
+++ Assignment 3.1/main.py
@@ -1,52 +1,46 @@
 import sys
-addedClauses = ()  # ~p q    q ~p  are logically equivalent
-# to hold the line # in the KB, to be able to check resolution rule against
-clausesChecked = ()
 
 
-
-
-def readInKB():
+def readInKB(OUTPUT_CNT):
     KBFile = open(sys.argv[1], "r")
     KB = []
+
     for line in KBFile.readlines():
         KB.append(line.strip())
 
-    return KB
+    for clause in KB[:-1]:
+        print(f"{OUTPUT_CNT}. {clause} " + "{}")
+        OUTPUT_CNT += 1
 
-def add_negation(kb, clause_list):
+    return KB, OUTPUT_CNT
+
+
+def add_negation(kb, clause_list, OUTPUT_CNT):
     kb = kb.copy()
+
     for literal in clause_list:
         if isRedundant(literal, kb):
             pass
         else:
             kb.append(literal)
-    return kb.copy()
+            output_line(kb, OUTPUT_CNT)
+            OUTPUT_CNT += 1
+    return kb.copy(), OUTPUT_CNT
 
 
 def resolutionApplicable(current_clause, clause_j):
-    if type(current_clause) == str:
-        current_clause = current_clause.split(" ")
-    if type(clause_j) == str:
-        clause_j = clause_j.split(" ")
-    for literal in current_clause:
-        if literalNegation(literal) in clause_j:
+    for literal in current_clause.split(" "):
+        if literalNegation(literal) in clause_j.split(" "):
             return True, literal
     return False, None
 
+
 def resolve(clause1, clause2, literal):
-    print(f"1: {clause1}, 2: {clause2}, literal: {literal}")
-    if type(clause1) == str:
-        clause1 = clause1.split(" ")
-    if type(clause2) == str:
-        clause2 = clause2.split(" ")
-    c1 = set(clause1)
-    c2 = set(clause2)
+    c1 = set(clause1.split(" "))
+    c2 = set(clause2.split(" "))
     c1.remove(literal)
     c2.remove(literalNegation(literal))
-    print(f"c1: {c1}, c2: {c2}, literal: {literal}")
     resolvedClause = c1.union(c2)
-    print(resolvedClause)
     return resolvedClause
 
 
@@ -72,9 +66,7 @@ def negateClause(clause):
 def isRedundant(currentClause, KB):
     # loop thru current clauses, and see if any of them match, regardless of order
     for clause in KB:
-        if type(clause) == str:
-            clause = clause.split(" ")
-
+        clause = clause.split(" ")
         if set(currentClause) == set(clause):
             return True
     return False
@@ -91,66 +83,63 @@ def isTrue(currentClause):
                 return True
     return False
 
-def literalNegation(literal_to_negate):  # return a string representing the negation of a particular literal
+
+# return a string representing the negation of a particular literal
+def literalNegation(literal_to_negate):
     if "~" in literal_to_negate:
         new_literal = literal_to_negate[1:]
     else:
-        new_literal = "~"+literal_to_negate
+        new_literal = "~" + literal_to_negate
     return new_literal
+
+
+def format_clause(clause):
+    clause_str = ""
+    cnt = len(clause)
+    for literal in clause:
+        clause_str += literal
+        clause_str += " " if cnt > 1 else ""
+        cnt -= 1
+    return clause_str
+
+
+def output_line(KB, OUTPUT_CNT, i=None, j=None, isNew=False):
+    index_str = ("{" + f"{i+1}, {j+1}" "}") if isNew else ""
+    output = f"{OUTPUT_CNT}. {KB[-1]} " + index_str
+    print(output)
+    return
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    KB = readInKB()
+    OUTPUT_CNT = 1
+    KB, OUTPUT_CNT = readInKB(OUTPUT_CNT)
 
     # negation contains array of negated clauses
     negation = negateClause(KB[-1])
-
     KB = KB[0:-1]
-    # TODO: add negation to KB?
-    KB = add_negation(KB, negation)
-    print(KB)
+    KB, OUTPUT_CNT = add_negation(KB, negation, OUTPUT_CNT)
 
-    ''' Resolution should proceed as follows: 
-        
-        For each clause i[1,n] (where n is the last clause in the KB), 
-            attempt to resolve clause i with every previous clause j[1,i) (in order). 
-        
-        If a new clause is generated, it is added to the end of the KB (therefore the value of n changes). 
-        Your system should continue trying to resolve the next clause (i+1) with all previous clauses until
-            1) a contradiction is found (in which case ’Contradiction’ should be added to the KB) or 
-            2) all possible resolutions have been performed.
-    '''
-
-    # MAIN_LOOP
-    # main for loop tries to find a j in order 0 to j that can be resolved with i
-    # then, resolve i with all j that can be resolved
-    # resolution should return one of 2: new clause, Fail
-    # seemingly expensive algorithm to find a j that can be resolved:
-    #   1. for all literals in i, see if literalNegation(literal) is in j
-    #   2. if not, resolution cannot be applied
-    #   3. if so, apply resolution and derive either failure or a new clause
-    #   4. if at any point there is a FAILURE, proof by negation has occurred and we can finish
     i = 0
     while i < len(KB):
         for j in range(0, i):
-            # resolve clause
-            # print("I, J: " + KB[i]+", " + KB[j])
-
             applicable, literal = resolutionApplicable(KB[i], KB[j])
             if applicable:
-                print(f"index 1: {i}, index 2: {j}")
-                clause = resolve(KB[i], KB[j], literal)  # resolve should likely have 2 parameters, clause i an clause j
-                # if clause == FAILURE, proof by negation successful, finish
+                clause = resolve(KB[i], KB[j], literal)
                 clause = removeRepeatedLiterals(clause)
+
                 if isRedundant(clause, KB):
                     pass
                 elif isTrue(clause):
                     pass
                 else:
                     if len(clause) == 0:
-                        print("Contradiction Found")
+                        print("Contradiction {" + f"{i+1}, {j+1}" + "}")
+                        print("Valid")
                         exit()
-                    KB.append(clause)
-                    print(f"new KB: {KB}\n")
+
+                    KB.append(format_clause(clause))
+                    output_line(KB, OUTPUT_CNT, i, j, True)
+                    OUTPUT_CNT += 1
         i += 1
 #  end for
